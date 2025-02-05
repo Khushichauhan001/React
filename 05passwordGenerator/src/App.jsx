9725cd9b5a3a6d032485c4da28c0a6c8e417c310
Khushichauhan01 <khushichauhan2506@gmail.com>
2025-02-05 10:36:09 +0530

    add password generator project

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState , useCallback} from 'react'
+import { useState , useCallback, useEffect, useRef} from 'react'
 // import './App.css'
 
 function App() {
@@ -6,6 +6,9 @@ function App() {
   const [numberAllowed , setNumberAllowed] = useState(false);
   const [charAllowed , setCharAllowed] = useState(true);
   const [password , setPassword] = useState("")   // it is used kyuki isme bhi toh state chnge hogi har br new password generate krne k lie 
+
+  //useRef hook
+  const passwordRef = useRef(null)
 
 
   // password generator 
@@ -15,35 +18,95 @@ function App() {
     if(numberAllowed) str += "0123456789"
     if(charAllowed) str += "!@#$%^&*_-+={}[]~`"
 
-        for(let i=1 ; i<= array.length ; i++){
-          let char = Math.floor(Math.random() * str.length + 1 )
-          pass = str.charAt(char) // loop khtm hote hi pass m value aajygi .. but sirf value aaygi ..read ni hogi uske lie setpassword m s pass value pass krni hogi
+
+//copy to keyboard (firstly select the portion , then range ki kitna copy krna h .. and also access of keyboard )
+          for(let i=1 ; i<= length ; i++){
+          let char = Math.floor(Math.random() * str.length )
+          pass += str.charAt(char) // loop khtm hote hi pass m value aajygi .. but sirf value aaygi ..read ni hogi uske lie setpassword m s pass value pass krni hogi  
         }
 
         setPassword(pass)  // it read the password 
+}, [length , numberAllowed , charAllowed , setPassword])  // useCallback(fn , dependencies)
+// does not compare its dependencies with useeffect() depen... yha hum optimization k lie dependencies lga rhe h  .. memorization conceot aajyga yha ... ki memory  m rkho setpassword ko bhi o baki sbko 
 
 
 
-  } , [length , numberAllowed , charAllowed , setPassword])  // useCallback(fn , dependencies)
+const copyPasswordToClipboard = useCallback(() => {
+  // doesnot need neeche bali ek line ... 
+  passwordRef.current?.select() ;  // just to increase user experience in ui 
+  passwordRef.current?.setSelectionRange(0,100);
+  window.navigator.clipboard.writeText(password)
+},[password])
+
+  // method to generate password 
+  // ek method ho skta h ki button bnaye or fir usme function call krde 
+  // second methd can be ki aap hook bnA SKTE H (useEffect hook)
+
+  
+ useEffect(() => {
+  passwordGenerator()
+},[length , numberAllowed , charAllowed , passwordGenerator])  // or yha hum islie lga rhe h ki kuch bhi chnges ho toh dubara s run ho code 
 
 
 
   return (
-    <>
-    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-700 bg-gray-700'>
+    
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-black
+     bg-gray-700">
 
-      <h1 className='tect-4xl text-center text-white'> PASSWORD GENERATOR</h1>
+      <h1 className='text-4xl text-center text-white my-3'> PASSWORD GENERATOR</h1>
 
-      <div className='className="flex shadow rounded-lg overflow-hidden mb-4" '>
+      <div className="flex shadow rounded-lg overflow-hidden mb-4">
         <input
          type="text"
          value={password}
-         className='outline-none w-full py-1 px-3 rounded-md' placeholder='password' readOnly
+         className="outline-none w-full py-1 px-3" placeholder="Password" readOnly
+         ref={passwordRef}
         />
-    </div>
-</div>
 
-    </>
+        <button 
+        onClick={copyPasswordToClipboard }
+        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0' > copy </button>
+    </div>
+
+    <div className='flex text-sm gap-x-2'>
+    <div className='flex items-center gap-x-1'>
+      <input 
+      type="range"
+       min={5}
+       max={100}
+       value={length} 
+       className='cursor-pointer '
+       onChange={(e) =>{setLength(e.target.value)}}
+       />
+       <label> Length: {length} </label>
+    </div>
+
+    <div className='flex items-center gap-x-1'>
+      <input
+        type='checkbox' 
+        defaultChecked={numberAllowed}
+        id="numberInput"
+        onChange={() => {
+          setNumberAllowed((prev) => !prev);   // prev value se reverse krdo true or false 
+        }}
+      />
+      <label htmlFor='numberInput'>Numbers</label>
+       </div>
+
+       <div className=' flex items-center gap-x-1'>
+        <input 
+        type='checkbox'
+        defaultChecked={charAllowed}
+        id='characterInput'
+        onChange={() => {
+          setCharAllowed((prev) => !prev);  // callback fire it gives us new value opposite to last value 
+        }}
+        />
+        <label htmlFor="characterInput">Characters</label>
+       </div>
+ </div> 
+ </div>
   )
 }
 
